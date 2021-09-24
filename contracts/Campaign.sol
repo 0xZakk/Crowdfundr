a146518257ab16330802f42afa82f97815b897eb
Zakk Fleischmann <zakk@thinknimble.com>
2021-09-24 08:20:04 -0500

    second commit

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,53 +2,117 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 contract Campaign {
+  address payable public owner;
   string public title;
-  address public owner;
   uint public balance = 0;
   uint public createdAt;
   uint public endDate;
   uint public fundingGoal;
   uint private counter;
+  bool private active = true;
+
+  struct Contribution {
+    uint value;
+    uint date;
+    string tier;
+  }
 
   // track the NFT id and the address that made the
   // contribution:
-  mapping(uint => address) contributions;
+  mapping(uint => address) private awardOwners;
+  mapping(address => Contribution[]) private contributions;
+
+  event ContributionReceived(address indexed contributor, uint contributionAmount, string contributionTier);
+  event FundingComplete(string title, uint totalContributed);
+  event CampaignCancelled(string title, address indexed owner, string message);
+
+  modifier onlyOwner() {
+      require(owner == msg.sender, "Ownable: caller is not the owner");
+      _;
+  }
 
   constructor(string memory _title, uint _fundingGoal) {
-    // NOTE: How to make sure the date is in the future? Greater than now()
-    owner = msg.sender;
+    owner = payable(msg.sender);
     title = _title;
     createdAt = block.timestamp;
     endDate = createdAt + 30 days;
     fundingGoal = _fundingGoal;
   }
 
-  function ownerOf (uint tokenId) public view returns(address) {
-    return contributions[tokenId];
+  function totalContributions () public view returns(uint contributionTotal) {
+    Contribution[] memory senderContributions = contributions[msg.sender];
+    uint numberOfContributions = senderContributions.length;
+
+    for (uint i = 0; i < numberOfContributions; i++) {
+      contributionTotal += senderContributions[i].value;
+    }
+
+    return contributionTotal;
   }
+  
 
-  // function _mint (string memory _type, address _receiver) internal {
-  //   contributions[counter] = _receiver;
-  //   counter++;
-  // }
-
+  function getStatus () public view returns(bool campaignStatus) {
+    return active;
+  }
+  
+  function cancel (string memory _cancellationMessage) public onlyOwner {
+    active = false;
+    emit CampaignCancelled(title, msg.sender, _cancellationMessage);
+  }
+  
   function contribute() public payable {
+    require(active, "Campaign closed, no more contributions accepted.");
     require(msg.value > 0.01 ether, "The minimum contribution is 0.01 ether");
+    string memory contributionType;
+
     balance += msg.value;
+
+    if (msg.value >= 1 ether) {
+      contributionType = "Gold";
+    } else if (msg.value >= 0.25 ether) {
+      contributionType = "Silver";
+    } else {
+      contributionType = "Bronze";
+    }
+
+    _mint(contributionType, msg.sender);
+
+    emit ContributionReceived(msg.sender, msg.value, contributionType);
+
+    if(balance >= fundingGoal) {
+      active = false;
+      emit FundingComplete(title, balance);
+    }
   }
+
+  function withdraw () public onlyOwner {
+    owner.transfer(balance);    
+  }
+
+  function withdraw (uint amount) public onlyOwner {
+    owner.transfer(amount);    
+  }
+
+  function _mint (string memory _type, address _receiver) internal {
+    Contribution memory contribution = Contribution({
+      value: msg.value,
+      date: block.timestamp,
+      tier: _type
+    });
+    awardOwners[counter] = _receiver;
+    contributions[_receiver].push(contribution);
+    counter++;
+  }
+
+  function ownerOf (uint tokenId) public view returns(address) {
+    return awardOwners[tokenId];
+  }
+
+  function balanceOf () public view returns(uint numberOfContributions) {
+    return contributions[msg.sender].length;
+  }
+  
 }
 
-    // string type;
 
-    // if (msg.value > 1 ether) {
-    //   type = "Gold";
-    // } else if (msg.value > 0.25 ether) {
-    //   type = "Silver";
-    // } else {
-    //   type = "Bronze";
-    // }
-
-    // _mint(type, msg.sender);
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -16,7 +16,7 @@ describe("Campaign contract", function () {
   let addrs;
   let testData = {
     title: "Test Campaign",
-    fundingGoal: 10,
+    fundingGoal: parseEther("10"),
   };
 
   beforeEach(async function () {
@@ -49,9 +49,7 @@ describe("Campaign contract", function () {
     });
 
     it("Should set the funding goal", async function () {
-      expect(await campaign.fundingGoal()).to.equal(
-        testData.fundingGoal
-      );
+      expect(await campaign.fundingGoal()).to.equal(testData.fundingGoal);
     });
   });
 
@@ -62,97 +60,212 @@ describe("Campaign contract", function () {
       ).to.be.revertedWith("The minimum contribution is 0.01 ether");
     });
 
-    it('Should take contributions and increase the balance', async function (done) {
-      const value = parseEther("1")
-      await campaign.contribute({ value })
-      const newBalance = await campaign.balance()
+    it("Should take contributions and increase the balance", async function () {
+      const value = parseEther("1");
+      await campaign.contribute({ value });
+      const newBalance = await campaign.balance();
 
-      console.log(newBalance.eq(value))
-      // expect(newBalance.eq(value)).to.be.true
-      expect(newBalance).to.equal(value)
+      expect(newBalance).to.equal(value);
     });
 
-  //   it('Should accept contributions from the creator/owner', function (done) {
+    it("Should accept people aping TF in with all their NFT winnings", async function () {
+      const value = parseEther("100");
+      await campaign.connect(addr1).contribute({ value });
+      const balance = await campaign.balance();
 
-  //   });
+      expect(balance).to.equal(value);
+    });
 
-  //   it('Should accept more than contribution from the same address', function (done) {
+    it("Should emit the ContributionReceived event", async function () {
+      const value = parseEther("1");
+      await expect(campaign.contribute({ value }))
+        .to.emit(campaign, "ContributionReceived")
+        .withArgs(owner.address, value, "Gold");
+    });
 
-  //   });
+    it("Should return the number of contributions of an address", async function () {
+      await campaign.contribute({ value: parseEther("1.25") });
+      await campaign.contribute({ value: parseEther(".25") });
 
-  // it('Should emit a Contribution event', function (done) {
+      const contributions = await campaign.balanceOf();
 
-  // });
-  })
+      await expect(contributions.toNumber()).to.equal(2);
+    });
+
+    it("Should return the total contribution (value) of an address", async function () {
+      const cont1 = parseEther("1");
+      const cont2 = parseEther("2");
+      const cont3 = parseEther("3");
+
+      await campaign.connect(addr1).contribute({ value: cont1 });
+      await campaign.connect(addr1).contribute({ value: cont2 });
+      await campaign.connect(addr1).contribute({ value: cont3 });
+
+      const contributions = await campaign.connect(addr1).totalContributions();
+
+      await expect(contributions).to.equal(parseEther("6"));
+    });
+  });
 
   // describe("Tiers", function () {
-  //   it('Should return a Bronze tier NFT for base contributions', function (done) {
+  //   it('Should return a Bronze tier NFT for base contributions', function () {
 
   //   });
 
-  //   it('Should return a Silver NFT for contributions above 0.25 Eth', function (done) {
+  //   it('Should return a Silver NFT for contributions above 0.25 Eth', function () {
 
   //   });
 
-  //   it('Should return a Gold tier NFT for contributions above 1 Eth', function (done) {
+  //   it('Should return a Gold tier NFT for contributions above 1 Eth', function () {
 
   //   });
   // })
 
-  // describe('Campaign Management', function () {
-  //   it('Should be cancelable by the owner', function (done) {
+  describe("Campaign Management", function () {
+    it("Should be able to get the campaign's status", async function () {
+      const status = await campaign.getStatus();
+      expect(status).to.be.true;
+    });
 
-  //   });
+    it("Should be cancelable by the owner", async function () {
+      await campaign.cancel("Don't want to do it anymore.");
+      const status = await campaign.getStatus();
+      await expect(status).to.be.false;
+    });
 
-  //   it('Should not be cancelable by not the owner', function (done) {
+    it("Should only be cancelable by the owner", async function () {
+      await expect(
+        campaign.connect(addr1).cancel("Don't want to do it anymore.")
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
 
-  //   });
+    it("Should emit a CampaignCancelled event", async function () {
+      await expect(campaign.cancel("Don't want to do it anymore."))
+        .to.emit(campaign, "CampaignCancelled")
+        .withArgs(
+          testData.title,
+          owner.address,
+          "Don't want to do it anymore."
+        );
+    });
 
-  // it('Should emit a CampaignCancelled event', function (done) {
+    it("Should block contributions once the campaign is cancelled", async function () {
+      await campaign.cancel("Don't want to do it anymore.");
+      await expect(
+        campaign.connect(addr1).contribute({ value: parseEther("2") })
+      ).to.be.revertedWith("Campaign closed, no more contributions accepted.");
+    });
+  });
 
-  // });
-  // });
+  describe("Funding", function () {
+    it("Should set the status to inactive when the funding goal is met", async function () {
+      await campaign.contribute({
+        value: parseEther("10"),
+      });
 
-  // describe('Funding', function () {
-  //   it('Should set status to funded when the funding goal is met', function (done) {
+      const active = await campaign.getStatus();
+      expect(active).to.be.false;
+    });
 
-  //   });
+    it("Should be reject contributions when the funding goal is met", async function () {
+      await campaign.contribute({
+        value: parseEther("10"),
+      });
 
-  //   it('Should be reject contributions when the funding goal is met', function (done) {
+      await expect(
+        campaign.connect(addr1).contribute({ value: parseEther("1") })
+      ).to.be.revertedWith("Campaign closed, no more contributions accepted.");
+    });
 
-  //   });
+    it("Should emit a FundingComplete event", async function () {
+      await expect(campaign.contribute({ value: parseEther("10") }))
+        .to.emit(campaign, "FundingComplete")
+        .withArgs(testData.title, parseEther("10"));
+    });
 
-  // it('Should emit a FundingComplete event', function (done) {
+    it("Should allow raising past the funding goal", async function () {
+      const value = parseEther("100");
+      await campaign.contribute({ value });
 
-  // });
+      const active = await campaign.getStatus();
 
-  //   it('Should be able to withdraw when funding goal is met', function (done) {
+      const balance = await campaign.balance();
 
-  //   });
+      expect(active).to.be.false;
+      expect(balance).to.equal(value);
+    });
 
-  //   it('Should block withdraw from everyone except the owner', function (done) {
+    it("Should be able to withdraw when funding goal is met", async function () {
+      const value = parseEther("15");
+      await campaign.connect(addr1).contribute({ value });
 
-  //   });
+      await expect(await campaign["withdraw()"]).to.changeEtherBalance(
+        owner,
+        value
+      );
+    });
 
-  //   it('Should be able to withdraw part of the balance', function (done) {
+    it("Should block withdraw from everyone except the owner", async function () {
+      const value = parseEther("15");
+      await campaign.connect(addr1).contribute({ value });
 
-  //   });
-  // });
+      await expect(campaign.connect(addr1)['withdraw()']()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    // it("Should be able to withdraw part of the balance", async function () {
+    //   const withdrawalAmmount = parseEther("5");
+    //   const contributionAmmount = parseEther("15")
+
+    //   await campaign.contribute({ value: contributionAmmount });
+
+    //   console.log("withdrawalAmmount", formatEther(withdrawalAmmount))
+    //   console.log("contributionAmmount", formatEther(contributionAmmount))
+
+    //   const startingBalance = await campaign.balance()
+    //   console.log("startingBalance", formatEther(startingBalance))
+
+    //   await campaign['withdraw(uint256)'](withdrawalAmmount)
+
+    //   const secondBalance= await campaign.balance()
+    //   console.log("secondBalance", formatEther(secondBalance))
+
+    //   // await expect(
+    //   //   await campaign['withdraw(uint256)'](withdrawalAmmount)
+    //   // ).to.changeEtherBalance(owner, withdrawalAmmount);
+
+    //   // const diff = contributionAmmount.sub(withdrawalAmmount)
+    //   // await expect(campaign.balance()).to.equal(diff);
+    // });
+
+    // it('Should return the balance if the withdrawal amount exceeds the balance', async function () {
+    //   const contributionAmmount = parseEther("15")
+    //   const withdrawalAmmount = parseEther("20");
+
+    //   await campaign.contribute({ value: contributionAmmount })
+
+    //   await expect(
+    //     await campaign['withdraw(uint256)'](withdrawalAmmount)
+    //   ).to.changeEtherBalance(owner, contributionAmmount);
+
+    // });
+  });
 
   // describe('Failed', function () {
-  //   it('Should set status to failed if funding goal is not met in 30 days', function (done) {
+  //   it('Should set status to failed if funding goal is not met in 30 days', async function () {
 
   //   });
 
-  // it('Should emit a FundingFailed event', function (done) {
+  // it('Should emit a FundingFailed event', async function () {
 
   // });
 
-  //   it('Should block contributions after the end date has passed', function (done) {
+  //   it('Should block contributions after the end date has passed', async function () {
 
   //   });
 
-  //   it('Should refund contributions if the funding goal is not met', function (done) {
+  //   it('Should refund contributions if the funding goal is not met', async function () {
 
   //   });
   // });
